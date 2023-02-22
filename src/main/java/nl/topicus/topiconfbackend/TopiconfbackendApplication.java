ad39b43fed2c61a3455792fa2c5dbff0aa862897
Roger Wolf <roger.a.wolf@hotmail.nl>
2023-02-22 11:44:55 +0100

    first commit

---
+++ src/main/java/nl/topicus/topiconfbackend/TopiconfbackendApplication.java
@@ -0,0 +1,13 @@
+package nl.topicus.topiconfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopiconfbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopiconfbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/topicus/topiconfbackend/TopiconfbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.topicus.topiconfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopiconfbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
