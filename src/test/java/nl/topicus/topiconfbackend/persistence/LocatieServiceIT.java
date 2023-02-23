e06fb5fd0be0614c5a428b416780708e2b4aca79
RLM <r.lemges@workingspirit.nl>
2023-02-23 16:46:08 +0100

    Test Classes toegevoegd

---
+++ src/test/java/nl/topicus/topiconfbackend/persistence/LocatieServiceIT.java
@@ -0,0 +1,48 @@
+package nl.topicus.topiconfbackend.persistence;
+
+import nl.topicus.topiconfbackend.domain.Locatie;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LocatieServiceIT {
+
+    @InjectMocks
+    private LocatieService locatieService;
+
+    @Mock
+    private LocatieRepository locatieRepository;
+
+    public LocatieServiceIT() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testFindById() {
+
+        // Given
+        Locatie mockedLocatie = new Locatie();
+        mockedLocatie.setId(1L);
+        mockedLocatie.setName("Rudi");
+        mockedLocatie.setDescription("Een mooie rustgevende locatie");
+        mockedLocatie.setCapacity("100 Personen");
+        Optional<Locatie> myLocatie = Optional.of(mockedLocatie);
+        Mockito.when(this.locatieRepository.findById(1L)).thenReturn(myLocatie);
+
+        // When
+        Locatie locatieFromService = this.locatieService.findById(1L);
+
+        // Then
+        assertNotNull(locatieFromService);
+        assertEquals(1L, locatieFromService.getId());
+        assertEquals("Rudi", locatieFromService.getName());
+        assertEquals("Een mooie rustgevende locatie", locatieFromService.getDescription());
+        assertEquals("100 Personen", locatieFromService.getCapacity());
+
+    }
+}
---
+++ src/test/java/nl/topicus/topiconfbackend/rest/LocatieEndpointIT.java
@@ -0,0 +1,42 @@
+package nl.topicus.topiconfbackend.rest;
+
+import nl.topicus.topiconfbackend.domain.Locatie;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("intergrationtest")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class LocatieEndpointIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private static long lastid = -1;
+
+    @Test
+    @Order(1)
+    public void testPost() {
+
+        // Given
+        Locatie locatieToBePosted = new Locatie();
+
+        // When
+        ResponseEntity<Locatie> result = this.restTemplate.postForEntity("/locatie/voegLocatieToe", locatieToBePosted, Locatie.class);
+
+        // Then
+        assertEquals(200, result.getStatusCode().value());
+
+        // Get en Delete testen hieronder nog toevoegen
+
+    }
+}
