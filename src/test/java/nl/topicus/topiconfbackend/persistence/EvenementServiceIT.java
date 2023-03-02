d497e2e12935fcdff32bc4894b3eae90fe738720
RLM <r.lemges@workingspirit.nl>
2023-03-02 18:43:08 +0100

    Parameterized Test toegevoegd

--- src/test/java/nl/topicus/topiconfbackend/persistence/EvenementServiceIT.java
+++ src/test/java/nl/topicus/topiconfbackend/persistence/EvenementServiceIT.java
@@ -1,14 +1,33 @@
 package nl.topicus.topiconfbackend.persistence;
 
+import nl.topicus.topiconfbackend.domain.Aanvraag;
 import nl.topicus.topiconfbackend.domain.Evenement;
 import nl.topicus.topiconfbackend.domain.Locatie;
 import nl.topicus.topiconfbackend.domain.Track;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class EvenementServiceIT {
+
+    private LocalDateTime mockedDateTime;
+    private List<Track> mockedTrackList;
+    private List<Aanvraag> mockedAanvraagList;
 
     @InjectMocks
     private EvenementService evenementService;
@@ -19,6 +38,9 @@ public class EvenementServiceIT {
     // Constructor
     public EvenementServiceIT() {
         MockitoAnnotations.openMocks(this);
+        mockedDateTime = LocalDateTime.now();
+        mockedTrackList = new ArrayList<>();
+        mockedAanvraagList = new ArrayList<>();
     }
 
     @Test
@@ -51,9 +73,37 @@ public class EvenementServiceIT {
         // To be programmed
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(longs = 1L)
     public void testfindById(long eventID) {
-        // To be programmed
+
+        // Given
+        Evenement mockedEvenement = new Evenement();
+        mockedEvenement.setId(eventID);
+        mockedEvenement.setNaam("Jan Smit");
+        mockedEvenement.setOmschrijving("Ongetwijfeld Nederlands beste volkszanger");
+        mockedEvenement.setBeginDatumTijd(mockedDateTime);
+        mockedEvenement.setEindDatumTijd(mockedDateTime);
+        mockedEvenement.setTrackList(mockedTrackList);
+        mockedEvenement.setAanvraagList(mockedAanvraagList);
+
+        Optional<Evenement> myEvenement = Optional.of(mockedEvenement);
+
+        Mockito.when(this.evenementRepository.findById(eventID)).thenReturn(myEvenement);
+
+        // When
+        Evenement evenementFromService = this.evenementService.findById(eventID);
+
+        // Then
+        assertNotNull(evenementFromService);
+        assertEquals(1L, evenementFromService.getId());
+        assertEquals("Jan Smit", evenementFromService.getNaam());
+        assertEquals("Ongetwijfeld Nederlands beste volkszanger", evenementFromService.getOmschrijving());
+        assertEquals(mockedDateTime , evenementFromService.getBeginDatumTijd());
+        assertEquals(mockedDateTime, evenementFromService.getEindDatumTijd());
+        assertEquals(mockedTrackList, evenementFromService.getTrackList());
+        assertEquals(mockedAanvraagList, evenementFromService.getAanvraagList());
+
     }
 
     @Test
