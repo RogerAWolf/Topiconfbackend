b8d6dea3b1a515c9fd29dc39d62147efc57b70c1
Your Name <you@example.com>
2023-03-07 13:47:58 +0100

    mc

--- src/main/java/nl/topicus/topiconfbackend/persistence/PersoonRepository.java
+++ src/main/java/nl/topicus/topiconfbackend/persistence/PersoonRepository.java
@@ -2,10 +2,9 @@ package nl.topicus.topiconfbackend.persistence;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
-
 import nl.topicus.topiconfbackend.domain.Persoon;
 
 @Component
 public interface PersoonRepository extends CrudRepository<Persoon, Long>{
-
 }
+
--- src/main/java/nl/topicus/topiconfbackend/rest/PersoonEndpoint.java
+++ src/main/java/nl/topicus/topiconfbackend/rest/PersoonEndpoint.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+<<<<<<<< HEAD:src/main/java/nl/topicus/topiconfbackend/rest/PersoonEndpoint.java
 import nl.topicus.topiconfbackend.domain.Persoon;
 import nl.topicus.topiconfbackend.persistence.PersoonService;
 
@@ -24,12 +25,23 @@ public class PersoonEndpoint {
 
 	@Autowired
 	PersoonService as;
+========
+import nl.topicus.topiconfbackend.domain.Voorstel;
+import nl.topicus.topiconfbackend.persistence.VoorstelService;
+
+@RestController
+public class VoorstelEndPoint {
+
+	@Autowired
+	VoorstelService as;
+>>>>>>>> main:src/main/java/nl/topicus/topiconfbackend/rest/VoorstelEndPoint.java
 	
 	//add request to database
 	 //fronted will make sure that all fields are filled
 	
 	@CrossOrigin
 	@PostMapping("/postBody")
+<<<<<<<< HEAD:src/main/java/nl/topicus/topiconfbackend/rest/PersoonEndpoint.java
 	public void toevoegenPersoon(@RequestBody Persoon persoon) {
 		as.toevoegenPersoon(persoon);
 	}
@@ -37,20 +49,42 @@ public class PersoonEndpoint {
 	@GetMapping("/getBody")
 	public Iterable<Persoon> bekijkenPersoon() {
 		return as.bekijkPersoon();
+========
+	public void toevoegenVoorstel(@RequestBody Voorstel voorstel) {
+		as.toevoegenVoorstel(voorstel);
+	}
+	
+	@GetMapping("/getBody")
+	public Iterable<Voorstel> bekijkenVoorstel() {
+		return as.bekijkVoorstel();
+>>>>>>>> main:src/main/java/nl/topicus/topiconfbackend/rest/VoorstelEndPoint.java
 	}
 	
 	//not able to use yet
 	@CrossOrigin
 	@PutMapping("/updateElementStatus/{id}")
+<<<<<<<< HEAD:src/main/java/nl/topicus/topiconfbackend/rest/PersoonEndpoint.java
 	public void selecterenEnUpdate(@PathVariable long id, @RequestBody Persoon persoon) {
 		as.toevoegenPersoon(persoon);
+========
+	public void selecterenEnUpdate(@PathVariable long id, @RequestBody Voorstel voorstel) {
+		as.toevoegenVoorstel(voorstel);
+>>>>>>>> main:src/main/java/nl/topicus/topiconfbackend/rest/VoorstelEndPoint.java
 	}
 	
 	
 	@CrossOrigin
 	@GetMapping("/getElement/{id}")
+<<<<<<<< HEAD:src/main/java/nl/topicus/topiconfbackend/rest/PersoonEndpoint.java
 	public Persoon getById(@PathVariable long id){
 		return as.findById(id);
 	}
 
+========
+	public Voorstel getById(@PathVariable long id){
+		return as.findById(id);
+	}
+	
+	
+>>>>>>>> main:src/main/java/nl/topicus/topiconfbackend/rest/VoorstelEndPoint.java
 }
