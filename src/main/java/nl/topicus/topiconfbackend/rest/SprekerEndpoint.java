606abb338ea9e970f94804682a72a544e06c587b
Your Name <you@example.com>
2023-03-07 14:35:47 +0100

    7maartpt2

--- src/main/java/nl/topicus/topiconfbackend/rest/SprekerEndpoint.java
+++ src/main/java/nl/topicus/topiconfbackend/rest/SprekerEndpoint.java
@@ -1,5 +1,6 @@
 package nl.topicus.topiconfbackend.rest;
 
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.topicus.topiconfbackend.domain.Spreker;
