05d24aaffd8b57fbac40b2550b00eb3baa397207
Your Name <you@example.com>
2023-03-08 10:11:07 +0100

    voorbeeld

---
+++ src/main/java/nl/topicus/topiconfbackend/persistence/AdministratorService.java
@@ -0,0 +1,29 @@
+package nl.topicus.topiconfbackend.persistence;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import nl.topicus.topiconfbackend.domain.Administrator;
+
+
+@Service
+public class AdministratorService {
+
+	@Autowired
+	AdministratorRepository ar;
+
+	public void toevoegenAdministrator(Administrator administrator) {
+		ar.save(administrator);
+		
+	}
+
+	public Iterable<Administrator> bekijkAdministrator() {
+		return ar.findAll();
+	}
+
+	public Administrator findById(long id){
+
+		 return ar.findById(id).get();
+	}
+
+	
+	}
---
+++ src/main/java/nl/topicus/topiconfbackend/rest/SprekerEndpoint.java
@@ -0,0 +1,56 @@
+package nl.topicus.topiconfbackend.rest;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import nl.topicus.topiconfbackend.domain.Spreker;
+import nl.topicus.topiconfbackend.persistence.SprekerService;
+
+
+@RestController
+public class SprekerEndpoint {
+	
+	
+
+
+	@Autowired
+	SprekerService as;
+	
+	//add request to database
+	 //fronted will make sure that all fields are filled
+	
+	@CrossOrigin
+	@PostMapping("/postBody")
+	public void toevoegenSpreker(@RequestBody Spreker spreker) {
+		as.toevoegenSpreker(spreker);
+	}
+	
+	@GetMapping("/getBody")
+	public Iterable<Spreker> bekijkenSpreker() {
+		return as.bekijkSpreker();
+	}
+	
+	//not able to use yet
+	@CrossOrigin
+	@PutMapping("/updateElementStatus/{id}")
+	public void selecterenEnUpdate(@PathVariable long id, @RequestBody Spreker spreker) {
+		as.toevoegenSpreker(spreker);
+	}
+	
+	
+	@CrossOrigin
+	@GetMapping("/getElement/{id}")
+	public Spreker getById(@PathVariable long id){
+		return as.findById(id);
+	}
+
+}
